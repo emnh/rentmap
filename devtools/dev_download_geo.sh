70571fb7a12f40deafb87d8d5a51ecc1f98ce353
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2011-05-16 22:48:39 +0200

    Add download geo cache tool.

---
+++ devtools/dev_download_geo.sh
@@ -0,0 +1,5 @@
+~/google_appengine/appcfg.py \
+    download_data \
+    --email=hvidevold@gmail.com --config_file=bulkloader.yaml \
+    --filename=data/geocache.xml --kind=DirectionsCache \
+    --url=http://localhost:8090/_ah/remote_api
